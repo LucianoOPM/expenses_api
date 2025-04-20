84c696d81dd7daa4778c517021490ae3b01f31f7
Luciano Perez <jesus-61914@hotmail.com>
2025-04-20 13:11:53 -0600

    UPDATE: updated version managing

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,4 +1,5 @@
 export default () => ({
   port: process.env.PORT || 8080,
+  versionApi: process.env.API_VERSION || 'v1',
   databaseUrl: process.env.DATABASE_URL,
 });
--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,14 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  app.setGlobalPrefix('api/v1');
+  const API_PORT = configService.get<number>('port');
+  const API_VERSION = configService.get<string>('versionApi');
+
+  app.setGlobalPrefix(`api/${API_VERSION}`);
   app.enableVersioning({
     type: VersioningType.URI,
   });
-  await app.listen(configService.get<number>('port'));
-  console.log(`Server is running on port ${configService.get<number>('port')}`);
+  await app.listen(API_PORT);
+  console.log(`Server is running on port ${API_PORT}`);
 }
 bootstrap();
